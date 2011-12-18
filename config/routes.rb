8ded1d9dad2613f6f75ee6826896bdf8dfc5f20e
vinod.jain <vinodj@diaspark.com>
2011-12-18 15:23:11 -0500

    added pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SampleApp::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,4 +59,14 @@ SampleApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+  #<% 
+  #=begin 
+  # <%= stylesheet_link_tag    "application" %> 
+  #  <%= javascript_include_tag "application" %>  
+  #<%= csrf_meta_tags %> 
+  #=end %>
+  
+  
+  
+  
 end
